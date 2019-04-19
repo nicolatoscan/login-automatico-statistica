ecdd26724d2c58d7593157ae1e18e37348fce658
Nicola Toscan <nicolatoscan99@outlook.it>
2019-04-19 10:28:51 +0200

    Initial commit

---
+++ login.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    document.getElementById('email_address').value = 'nicola.toscan';
+    document.getElementById('matricola').value = '202236';
+    document.getElementById("signin").click()
+}
