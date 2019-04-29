9c163b944aabecb10fc3ed121e58e1195cb559d3
Nicola Toscan <nicolatoscan99@outlook.it>
2019-04-29 21:41:41 +0200

    added get token

--- login.js
+++ login.js
@@ -3,3 +3,23 @@ window.onload = function () {
     document.getElementById('matricola').value = '202236';
     document.getElementById("signin").click()
 }
+
+
+setInterval(async function () {
+
+    let daRichiedere = false;
+    let today = new Date().toISOString().split("T")[0]
+    
+    if (localStorage.getItem("lastToken") !=  today) {
+        daRichiedere = true
+    }
+
+    if (daRichiedere) {
+        let el = document.getElementById('date')
+        if (el != null) {
+            el.value = today
+            document.getElementById('tokenrequest').click()
+            localStorage.setItem("lastToken", today)
+        }
+    }
+}, 200);
